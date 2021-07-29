50db33174927c3e5ef4c406da079f7f186d24262
Daniel Pagan <deapagan@gmail.com>
2021-07-29 09:25:21 -0500

    xmobar workspaces, colors, and muting audio

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -3,19 +3,21 @@ import           XMonad
 import           XMonad.Hooks.DynamicLog
 import           XMonad.Hooks.EwmhDesktops
 import           XMonad.Hooks.ManageHelpers
-import           XMonad.Layout.Magnifier
-import           XMonad.Layout.ThreeColumns
 import           XMonad.Util.EZConfig
 import           XMonad.Util.Ungrab
 
 main :: IO ()
-main = xmonad . ewmh =<< xmobar myConfig
+main = xmonad . ewmh =<< statusBar "xmobar" myXmobarPP toggleStrutsKey myConfig
+ where
+  toggleStrutsKey :: XConfig Layout -> (KeyMask, KeySym)
+  toggleStrutsKey XConfig { modMask = m } = (m, xK_b)
 
 myConfig =
   def { modMask         = mod4Mask
       , borderWidth     = 0
       , layoutHook      = myLayout
       , manageHook      = myManageHook
+      , workspaces      = myWorkspaces
       , handleEventHook = handleEventHook def <+> fullscreenEventHook
       }  -- Rebind Mod to the Super key
     `additionalKeysP` [ ("M-S-z", spawn "xsecurelock")
@@ -27,11 +29,25 @@ myConfig =
                       , ("M-S-<Return>"          , spawn "alacritty")
                       , ("<XF86AudioRaiseVolume>", spawn "pamixer -i 10")
                       , ("<XF86AudioLowerVolume>", spawn "pamixer -d 10")
+                      , ("<XF86AudioMute>"       , spawn "pamixer -t")
                       , ("<XF86MonBrightnessUp>" , spawn "brightnessctl s +10%")
                       , ( "<XF86MonBrightnessDown>"
                         , spawn "brightnessctl s 10%-"
                         )
                       ]
+
+myXmobarPP :: PP
+myXmobarPP = def { ppSep           = magenta " • "
+                 , ppTitleSanitize = xmobarStrip
+                 , ppCurrent       = wrap (blue "[") (blue "]")
+                 , ppUrgent        = red . wrap (yellow "!") (yellow "!")
+                 }
+ where
+  blue, magenta, red, yellow :: String -> String
+  magenta = xmobarColor "#c9b4cf" ""
+  blue    = xmobarColor "#81a2be" ""
+  yellow  = xmobarColor "#f0c674" ""
+  red     = xmobarColor "#cc6666" ""
 
 myManageHook :: ManageHook
 myManageHook = composeAll
@@ -40,10 +56,11 @@ myManageHook = composeAll
   , className =? "wired" --> doFloat
   ]
 
-myLayout = tiled ||| Mirror tiled ||| Full ||| threeCol
+myLayout = tiled ||| Mirror tiled ||| Full
  where
-  threeCol = magnifiercz' 1.3 $ ThreeColMid nmaster delta ratio
-  tiled    = Tall nmaster delta ratio
-  nmaster  = 1      -- Default number of windows in the master pane
-  ratio    = 1 / 2    -- Default proportion of screen occupied by master pane
-  delta    = 3 / 100  -- Percent of screen to increment by when resizing panes
+  tiled   = Tall nmaster delta ratio
+  nmaster = 1      -- Default number of windows in the master pane
+  ratio   = 1 / 2    -- Default proportion of screen occupied by master pane
+  delta   = 3 / 100  -- Percent of screen to increment by when resizing panes
+
+myWorkspaces = ["Web", "Emacs", "Slack"] ++ map show [4 .. 9]
